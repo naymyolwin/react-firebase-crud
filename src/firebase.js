59ea1bf4c512f524727e77cd7e760c9c96fcafba
Nay Myo Lwin <naymyolwin@gmail.com>
2021-04-02 18:38:53 +0700

    install firebase and added firebase.js with config

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKrWm6AWBbFzCiG_Z3Yic0uZSmD0w23dg",
+  authDomain: "react-firebase-crud-de718.firebaseapp.com",
+  projectId: "react-firebase-crud-de718",
+  storageBucket: "react-firebase-crud-de718.appspot.com",
+  messagingSenderId: "710784419944",
+  appId: "1:710784419944:web:845bee3740ef4a4d6c93dd",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
